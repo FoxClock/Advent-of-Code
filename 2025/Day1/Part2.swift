aa90b0dc6a47ee3f637f4f5573d078fc61ba1dbe
FoxClock <foxwellhayden@gmail.com>
2025-12-17 17:45:43 +1100

    Completed Day 1 and working on day 2

---
+++ 2025/Day1/Part2.swift
@@ -0,0 +1,113 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+@main
+struct Day1 {
+    static func main() {
+
+        // Constants
+        let mode: Mode
+        let input: String
+        let DialPostion: Int = 50
+
+        // Set debug or run mode
+        mode = Mode.from(CommandLine.arguments)
+
+        // read file contents
+        do { 
+            input = try readFile(mode.fileName)
+        } catch {
+            print("Cannot read file contents: \(error)")
+            exit(EXIT_FAILURE)
+        }
+
+        // Divide string into individual lines, convert to string not subsequence
+        let splitInput = input.split(separator: "\n")
+            .map({String($0)})
+
+        // Make values positive or negative
+        let alignedValues = scaleValues(splitInput)
+
+        if mode == .test {
+            print("Alignted Values: \(alignedValues)")
+        }
+
+        // get all rotations
+        let rotationPositions = applyRotations(start: DialPostion, rotations: alignedValues)
+
+        if mode == .test {print("Rots: \(rotationPositions)")}
+
+        let zeroCount = rotationPositions.count(where: {$0 == 0})
+
+        //print(rotationPositions)
+        print("Final result: \(zeroCount)")
+
+   }
+}
+
+enum Mode {
+    case test
+    case run
+
+    var fileName: String {
+        switch self {
+            case .test: return "test_input.txt"
+            case .run: return "input.txt"
+        }
+    }
+
+    static func from(_ args: [String]) -> Mode {
+        if args.contains("debug") {
+            return .test
+        } else {
+            return .run
+        }
+    }
+}
+
+func readFile(_ inputFile: String) throws -> String {
+    let url = URL(fileURLWithPath: inputFile)
+    return try String(contentsOf: url, encoding: .utf8)
+}
+
+
+func scaleValues(_ input: [String]) -> [Int] {
+    let output: [Int]
+
+    output = input.compactMap { inputValue in
+        if inputValue.contains("R") {
+            let newVal = inputValue.dropFirst()
+            return Int(newVal)
+        } else {
+            let newVal = inputValue.dropFirst()
+            return (-1 * (Int(newVal) ?? 0))
+        }
+    }
+
+    return output
+}
+
+func rotateDial(currentValue: Int, rotation: Int, dialSize: Int = 99) -> (Int, Int) {
+    let totalDialPositions = dialSize + 1
+    let absoluteRotations = currentValue + rotation
+    let fullRotations = absoluteRotations / totalDialPositions
+    let finalRotation = (currentValue + (rotation) % totalDialPositions + totalDialPositions) % totalDialPositions
+
+    if finalRotation == 0 {
+        return (finalRotation, 0)
+    } else {
+        return (finalRotation, fullRotations)
+    }
+}
+
+func applyRotations(start: Int, rotations: [Int]) -> [Int] {
+
+    // Reduce the outline 
+    rotations.reduce(into: [start]) {acc, rotation in
+        let result = rotateDial(currentValue: acc.last ?? 0, rotation: rotation)
+        let (nextValue, zeroCount) = result
+        for _ in 0..<zeroCount {acc.append(0)}
+        acc.append(nextValue)
+    }
+}
