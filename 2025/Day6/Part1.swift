43fe923abb84e19b0b407503acc8c9d0ac7428a4
FoxClock <foxwellhayden@gmail.com>
2026-07-20 08:34:27 +1000

    Completed part 1 of Day 6 challenge.

---
+++ 2025/Day6/Part1.swift
@@ -0,0 +1,171 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+// =====================
+// Enums
+// =====================
+enum Mode {
+    case test
+    case run
+
+    var fileName: String {
+        switch self {
+        case .run: "input.txt"
+        case .test: "test_input.txt"
+        }
+    }
+
+    static func from(_ input: [String]) -> Mode {
+        if input.contains("debug") || input.contains("--debug") {
+            return .test
+        } else {
+            return .run
+        }
+    }
+}
+
+enum Operator {
+    case multiply
+    case add
+
+    var arithOperator: (_ x: [Int]) -> Int {
+        switch self {
+        case .add:
+            { $0.reduce(0, +) }
+        case .multiply:
+            { $0.reduce(1, *) }
+        }
+    }
+
+    static func from(strArithmeticSymbol: String) -> Operator {
+        switch strArithmeticSymbol {
+        case "+": return .add
+        case "*": return .multiply
+        default:
+            print("Unable to determine operator.")
+            exit(EXIT_FAILURE)
+        }
+    }
+}
+
+// =====================
+// MARK: Main Function
+// =====================
+@main
+struct Day6 {
+    static func main() {
+        let mode: Mode = Mode.from(CommandLine.arguments)
+        let fileContents = openFile(mode.fileName)
+
+        // Split up the contents and split off the arithmetic symbols
+        var stringArray: [[String]] = splitString(fileContents)
+
+
+        guard !(stringArray.isEmpty) else {
+            print("Empty file, cannot extract arithmetic symbols.")
+            return
+        }
+
+        let symbols: [String] = stringArray.removeLast()
+
+        // reshape the arrays so that they are lined up ready for
+        // processing with their associated symbol
+        let reshapedArrays = reshapeArray(stringArray)
+
+        if mode == .test {
+            testPrint(fileContents, stringArray)
+            print("--------------------------")
+            print(reshapedArrays)
+        }
+
+        // Convert the reshaped arrays to integers
+        let convertedArrays = reshapedArrays.map({
+            $0.compactMap({ Int($0) })
+        })
+
+        var total = 0
+        for (array, arithOperator) in zip(convertedArrays, symbols) {
+            let op = Operator.from(strArithmeticSymbol: arithOperator)
+            let result = op.arithOperator(array)
+            total += result
+        }
+
+        print("Sum Total: \(total)")
+    }
+}
+
+// =====================
+// MARK: Auxiliary  Functions
+// =====================
+func openFile(_ fileName: String) -> String {
+
+    if fileName.isEmpty {
+        print("Unable to open file.")
+        return ""
+    }
+
+    let url: URL = URL(fileURLWithPath: fileName)
+
+    do {
+        let contents = try String(contentsOf: url, encoding: .utf8)
+        return contents
+    } catch {
+        return ""
+    }
+}
+
+func splitString(_ input: String) -> [[String]] {
+    let initialSplit = input.split(whereSeparator: { $0.isNewline })
+    let secondSplit = initialSplit.map(
+        { $0.split(separator: " ") }
+    )
+    let finalArray = secondSplit.map(
+        {
+            $0.compactMap(
+                { String($0) }
+            )
+        }
+    )
+
+    return finalArray
+}
+
+func reshapeArray(_ stringArrays: [[String]]) -> [[String]] {
+    var outputArrays: [[String]] = []
+
+    let yShape = stringArrays.count
+    guard let xShape = stringArrays.first?.count else {
+        print("The first array has no elements. Check inputs.")
+        return outputArrays
+    }
+
+    guard yShape == xShape else {
+        print("Array must have and equal number of rows and elements.")
+        return outputArrays
+    }
+
+    for x in 0..<xShape {
+        var outputArray: [String] = []
+        for y in 0..<yShape {
+            let value = stringArrays[y][x]
+            outputArray.append(value)
+        }
+        outputArrays.append(outputArray)
+    }
+
+    return outputArrays
+}
+
+// =====================
+// MARK: Test Input
+// =====================
+func testPrint(_ fileInput: String, _ StringArray: [[String]]) {
+    print("Test File input: \n--------------------------------------")
+    print(fileInput)
+    print("\nString Array:\n---------------------------------------")
+    for item in StringArray {
+        print("Item: \(item) - \(type(of: item))")
+    }
+}
